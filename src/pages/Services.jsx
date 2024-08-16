cde0bbd6fe91b724dda324ae7aa257c5b96839d0
starkethan <www.rahatsharma7@gmail.com>
2024-08-16 22:33:07 +0530

    updates

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useEffect} from 'react'
 // import { RWebShare } from "react-web-share";
 
@@ -10,63 +9,16 @@ export const Services = () => {
   })
 
 
-  
 
-// const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light')
-
-
-
- 
-  
-
-
-
-// useEffect(()=> {
-//   if(window.matchMedia('(prefers-color-scheme: dark)').matches){
-//     setTheme('dark');
-//   } 
-//   else {
-//     setTheme('light');
-//   }
-// }, [])
-
-// useEffect(() => {
-//   if (theme === 'dark') {
-//   localStorage.setItem("theme", theme);
-// document.documentElement.classList.add('dark')
-
-//   } else {
-//     localStorage.setItem("theme", theme);
-//     document.documentElement.classList.remove('dark')
-//   }
-// }, [theme]);
-
- 
-=======
-import React from 'react'
- 
-export const Services = () => {
->>>>>>> 85a7fe2e5c348c3a4c92391de8f38bdfa8a2bb07
   return (
     <div className='dark:bg-black dark:text-white '>
       <h1 className='text-6xl'>  No Services Available</h1> 
-<<<<<<< HEAD
 
 
-{/* <div>
-      <RWebShare
-        data={{
-          text: "Like humans, flamingos make friends for life",
-          url: "https://on.natgeo.com/2zHaNup",
-          title: "Flamingos",
-        }}
-      >
-        <button>Share 🔗</button>
-      </RWebShare>
-    </div> */}
 
 
-    {/* <button  onClick={() => {navigator.clipboard.writeText(currentPageUrl)}}>copy</button> */}
+
+    
 
 
 
@@ -76,27 +28,8 @@ export const Services = () => {
 
 
 
-{/* { theme === 'light' ? 
- <button onClick={toggle}>
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-20 h-20 ">
-  <path d="M12 2.25a.75.75 0 0 1 .75.75v2.25a.75.75 0 0 1-1.5 0V3a.75.75 0 0 1 .75-.75ZM7.5 12a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM18.894 6.166a.75.75 0 0 0-1.06-1.06l-1.591 1.59a.75.75 0 1 0 1.06 1.061l1.591-1.59ZM21.75 12a.75.75 0 0 1-.75.75h-2.25a.75.75 0 0 1 0-1.5H21a.75.75 0 0 1 .75.75ZM17.834 18.894a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 1 0-1.061 1.06l1.59 1.591ZM12 18a.75.75 0 0 1 .75.75V21a.75.75 0 0 1-1.5 0v-2.25A.75.75 0 0 1 12 18ZM7.758 17.303a.75.75 0 0 0-1.061-1.06l-1.591 1.59a.75.75 0 0 0 1.06 1.061l1.591-1.59ZM6 12a.75.75 0 0 1-.75.75H3a.75.75 0 0 1 0-1.5h2.25A.75.75 0 0 1 6 12ZM6.697 7.757a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 0 0-1.061 1.06l1.59 1.591Z" />
-</svg>
- </button>
 
-       :
-       <button onClick={toggle}>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-20 h-20">
-  <path fillRule="evenodd" d="M9.528 1.718a.75.75 0 0 1 .162.819A8.97 8.97 0 0 0 9 6a9 9 0 0 0 9 9 8.97 8.97 0 0 0 3.463-.69.75.75 0 0 1 .981.98 10.503 10.503 0 0 1-9.694 6.46c-5.799 0-10.5-4.7-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 0 1 .818.162Z" clipRule="evenodd" />
-</svg>
-       </button>
-    
-    } */}
-    
-
-
-
-=======
->>>>>>> 85a7fe2e5c348c3a4c92391de8f38bdfa8a2bb07
+  
 </div>
   )
 }
